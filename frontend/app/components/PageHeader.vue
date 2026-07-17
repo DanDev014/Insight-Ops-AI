d81c82da1c67bd322d16382ed2424ded78e06cd0
rovebelabanga <rovenelabanga2001@gmail.com>
2026-07-17 19:18:36 +0300

    feat: add reusable page header component

---
+++ frontend/app/components/PageHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col items-start gap-2">
+    <h3 class="font-bold">{{ props.title }}</h3>
+    <p class="text-gray-500 text-sm">{{ props.description }}</p>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+  description: string;
+}>();
+</script>
